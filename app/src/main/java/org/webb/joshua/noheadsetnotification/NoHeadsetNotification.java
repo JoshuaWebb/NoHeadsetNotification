80e8afeb1c9b29b17ea80706b8aae2ca5475768a
Joshua Webb <bbew.auhsoj+git@gmail.com>
2016-09-08 19:40:58 +1000

    Stop the notification.
     - Stop the "showHeadSetPlugin" method from being called.
    
      Found it through:
    
      https://gitlab.com/AOSPA-L/android_packages_services_Telecomm
      /blob/54c7754f7ddd958cfa62a2bd7afa94f21ad3620c/src/com/android
      /server/telecom/TtyManager.java

--- app/src/main/java/org/webb/joshua/noheadsetnotification/NoHeadsetNotification.java
+++ app/src/main/java/org/webb/joshua/noheadsetnotification/NoHeadsetNotification.java
@@ -1,12 +1,30 @@
 package org.webb.joshua.noheadsetnotification;
 
-import de.robv.android.xposed.IXposedHookZygoteInit;
+import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.XposedBridge;
+import de.robv.android.xposed.XposedHelpers;
+import de.robv.android.xposed.callbacks.XC_LoadPackage;
 
-public class NoHeadsetNotification implements IXposedHookZygoteInit {
+public class NoHeadsetNotification implements IXposedHookLoadPackage {
+
+	private final static String targetPackage = "com.android.server.telecom";
 
 	@Override
-	public void initZygote(StartupParam startupParam) throws Throwable {
-		XposedBridge.log("NoHeadsetNotification loaded");
+	public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
+		if (!targetPackage.equals(lpparam.packageName))
+			return;
+
+		String targetClassName = targetPackage + ".TtyManager";
+		Class<?> notifierClass = XposedHelpers.findClass(targetClassName, lpparam.classLoader);
+		XposedHelpers.findAndHookMethod(notifierClass, "showHeadSetPlugin",
+		  new XC_MethodHook() {
+			  @Override
+			  protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+				  super.beforeHookedMethod(param);
+				  param.setResult(null);
+			  }
+		  }
+		);
 	}
 }
